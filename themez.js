3ceca2cad257d260f0dc8d188b5b43f03738d8cc
dashdasnipa-alt <dashthehacker1911@gmail.com>
2025-02-28 13:38:11 -0800

    stuff

--- themez.js
+++ themez.js
@@ -1,17 +1,22 @@
 const currenttheme = document.getElementById("stupid-space-css");
 
-function themestuff() {
-    if ("theme" in localStorage) {
-        if (localStorage.getItem("theme") === "space") {
-            currenttheme.href="/themes/space.css"
-        } else if (localStorage.getItem("theme") === "matrix") {
-            currenttheme.href="/themes/hacker.css"
+document.addEventListener("DOMContentLoaded", () => {
+    function themestuff() {
+        if ("theme" in localStorage) {
+            if (localStorage.getItem("theme") === "space") {
+                currenttheme.href = "/themes/space.css";
+            } else if (localStorage.getItem("theme") === "matrix") {
+                currenttheme.href = "/themes/hacker.css";
+            }
+        } else {
+            localStorage.setItem("theme", "space");
+            themestuff();
         }
-    } else {
-        localStorage.setItem("theme", "space");
-        themestuff();
-    }    
-}
+        
+        if (currenttheme.href !== "") {
+            try {document.getElementById("hide").remove();} catch{}
+        }
+    }
 
-
-themestuff();
+    themestuff();
+});
