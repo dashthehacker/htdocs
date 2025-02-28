52625e744ebfc0936ef74aa4e54d9904e464b825
dashdasnipa-alt <dashthehacker1911@gmail.com>
2025-02-28 12:46:19 -0800

    theme stuff
    add a matrix theme and work on the theme switching

--- themez.js
+++ themez.js
@@ -1,7 +1,17 @@
 const currenttheme = document.getElementById("stupid-space-css");
 
-if ("theme" in localStorage) {
-    //alert("applying theme");
-} else {
-    localStorage.setItem("theme", "test");
+function themestuff() {
+    if ("theme" in localStorage) {
+        if (localStorage.getItem("theme") === "space") {
+            currenttheme.href="/themes/space.css"
+        } else if (localStorage.getItem("theme") === "matrix") {
+            currenttheme.href="/themes/hacker.css"
+        }
+    } else {
+        localStorage.setItem("theme", "space");
+        themestuff();
+    }    
 }
+
+
+themestuff();
